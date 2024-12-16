583d4b477ad317cb3bdc4badaebb0345720ebf9b
Luiza Kormann <109827640+luizakormann@users.noreply.github.com>
2024-12-16 12:35:27 -0300

    falta giant_line --strict

--- get_next_line.c
+++ get_next_line.c
@@ -12,66 +12,126 @@
 
 #include "get_next_line.h"
 
-char	*build_line(t_buf_mngr **rd_chrs, size_t line_len, int last_chr);
-
-char	*read_line_from_file(t_read_file *file)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	t_buf_mngr	*rd_chrs;
-	int			chr;
-	size_t		line_len;
+	size_t	len1;
+	size_t	len2;
+	char	*ptr;
 
-	rd_chrs = NULL;
-	line_len = 0;
-	while (1)
-	{
-		chr = read_next_char(file);
-		if (chr <= 0)
-			break ;
-		add_node(&rd_chrs, (char)chr);
-		line_len++;
-		if (chr == '\n')
-			break ;
-	}
-	if (!rd_chrs)
+	if (!s1 && !s2)
 		return (NULL);
-	return (build_line(&rd_chrs, line_len, chr));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	ptr = (char *)malloc(len1 + len2 + 1);
+	if (!ptr)
+		return (NULL);
+	ft_memcpy(ptr, s1, len1);
+	ft_memcpy(ptr + len1, s2, len2);
+	ptr[len1 + len2] = '\0';
+	return (ptr);
 }
 
-char	*build_line(t_buf_mngr **rd_chrs, size_t line_len, int last_chr)
+static int	read_and_process(int fd, char **remainder)
 {
-	char	*line;
+	char	*buffer;
+	char	*temp;
+	ssize_t	bytes_read;
 
-	line = write_str(*rd_chrs, line_len);
-	free_list(rd_chrs);
-	if (last_chr == -1)
+	if (!remainder) 
+        return (-1);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (-1);
+
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read < 0)
 	{
-		free(line);
-		return (NULL);
+		free(buffer);
+		return (-1);
 	}
+	if (bytes_read == 0)
+    {
+        free(buffer);
+        return (0);
+    }
+	buffer[bytes_read] = '\0';
+	if (!*remainder)
+		*remainder = ft_strdup("");
+
+	temp = ft_strjoin(*remainder, buffer);
+	free(buffer);
+	if (!temp)
+	{
+		free(*remainder);
+		*remainder = NULL;
+		return (-1);
+	}
+
+	free(*remainder);
+	*remainder = temp;
+	return (bytes_read);
+}
+
+static char	*extract_line(char **remainder)
+{
+	char	*newline;
+	char	*line;
+	char	*temp;
+
+	if (!remainder || !*remainder || **remainder == '\0')
+		return (NULL);
+	newline = NULL;
+	temp = NULL;
+	newline = ft_strchr(*remainder, '\n');
+	if (newline)
+	{
+		line = ft_substr(*remainder, 0, newline - *remainder + 1);
+		temp = ft_strdup(newline + 1);
+		if (!line || !temp)
+		{
+			free(line);
+			free(temp);
+			return (NULL);
+		}
+		free(*remainder);
+		*remainder = temp;
+		return (line);
+	}
+	line = ft_strdup(*remainder);
+	free(*remainder);
+	*remainder = NULL;
 	return (line);
 }
 
 char	*get_next_line(int fd)
 {
-	static t_read_file	file[1024];
-	char				*line;
+	static char	*remainder[1024];
+	char		*line;
+	int			read_result;
 
-	if (fd < 0 || fd >= 1024 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd >= 1024)
 		return (NULL);
-	if (file[fd].rd == 0 && file[fd].i == 0)
+	while (1)
 	{
-		file[fd].fd = fd;
-		file[fd].buf = malloc(BUFFER_SIZE);
-		if (!file[fd].buf)
+		if (remainder[fd] && ft_strchr(remainder[fd], '\n'))
+			break ;
+		read_result = read_and_process(fd, &remainder[fd]);
+		if (read_result < 0)
+		{
+			free(remainder[fd]);
+			remainder[fd] = NULL;
 			return (NULL);
+		}
+		if (read_result == 0)
+			break ;
 	}
-	line = read_line_from_file(&file[fd]);
+
+	line = extract_line(&remainder[fd]);
 	if (!line)
 	{
-		free(file[fd].buf);
-		file[fd].buf = NULL;
-		file[fd].i = 0;
-		file[fd].rd = 0;
+		free(remainder[fd]);
+		remainder[fd] = NULL;
 	}
+
 	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,85 +12,91 @@
 
 #include "get_next_line.h"
 
-t_buf_mngr	*create_node(char c)
+size_t	ft_strlen(const char *s)
 {
-	t_buf_mngr	*node;
+	size_t	i;
 
-	node = malloc(sizeof(t_buf_mngr));
-	if (!node)
-		return (NULL);
-	node->act_char = c;
-	node->next = NULL;
-	return (node);
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
-void	add_node(t_buf_mngr **head, char c)
+char	*ft_strchr(const char *s, int c)
 {
-	t_buf_mngr	*new_node;
-	t_buf_mngr	*current;
-
-	new_node = create_node(c);
-	if (!new_node)
-		return ;
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return ;
-	}
-	current = *head;
-	while (current->next)
-		current = current->next;
-	current->next = new_node;
-}
-
-void	*free_list(t_buf_mngr **head)
-{
-	t_buf_mngr	*current;
-	t_buf_mngr	*next;
-
-	if (!head || !*head)
+	unsigned char uc;
+	if (!s)
 		return (NULL);
-	current = *head;
-	while (current)
+	uc = (unsigned char)c;
+	while (*s)
 	{
-		next = current->next;
-		free(current);
-		current = next;
+		if (*(unsigned char *)s == uc)
+            return ((char *)s);
+        s++;
 	}
-	*head = NULL;
+	if (uc == '\0')
+        return ((char *)s);
 	return (NULL);
 }
 
-char	*write_str(t_buf_mngr *head, size_t len)
+char	*ft_strdup(const char *s)
 {
-	char	*line;
-	size_t	i;
+	size_t	slen;
+	char	*d;
 
-	line = malloc(len + 1);
-	if (!line)
+	if (!s)
 		return (NULL);
-	i = 0;
-	while (head)
-	{
-		line[i++] = head->act_char;
-		head = head->next;
-	}
-	line[i] = '\0';
-	return (line);
+	slen = ft_strlen(s);
+	d = (char *)malloc(slen + 1);
+	if (!d)
+		return (NULL);
+	ft_memcpy(d, s, slen);
+	d[slen] = '\0';
+	return (d);
 }
 
-int	read_next_char(t_read_file *file)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	if (file->i >= file->rd)
-	{
-		file->rd = read(file->fd, file->buf, BUFFER_SIZE);
-		if (file->rd < 0)
-			return (-1);
-		if (file->rd == 0)
-			return (0);
-		file->i = 0;
-	}
-	if (file->i < 0 || file->i >= BUFFER_SIZE)
-		return (-1);
-	return (file->buf[file->i++]);
+	char	*res;
+	size_t	slen;
+
+	if (!s)
+		return (NULL);
+	slen = ft_strlen(s);
+	if (start >= slen)
+		return (ft_strdup(""));
+	if (len > slen - start)
+		len = slen - start;
+	if (len > SIZE_MAX - 1)
+        return (NULL);
+	res = (char *)malloc(len + 1);
+	if (!res)
+		return (NULL);
+	if (ft_memcpy(res, s + start, len) == NULL)
+    {
+        free(res);
+        return (NULL);
+    }
+
+    res[len] = '\0';
+    return (res);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+
+	if (!dest || !src)
+		return (NULL);
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	if (dest != src)
+    {
+        while (n--)
+            *d++ = *s++;
+    }
+	return (dest);
 }
